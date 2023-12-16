5a76e153d9deadde05dc26790df71342fbfb9e09
Deepak Kumar Swami <63348699+dkswami@users.noreply.github.com>
2023-12-17 01:26:08 +0530

    ui dynamic done

--- src/components/puzzle/board/index.tsx
+++ src/components/puzzle/board/index.tsx
@@ -1,36 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "@chakra-ui/react";
 import Square from "../square";
 
 interface Props {}
 
+const defaultSquares = [
+	[null, null, null, null],
+	[null, null, null, null],
+	[null, null, null, null],
+	[null, null, null, null],
+]
+
 const Board: React.FC<Props> = ({}) => {
-  const handleSquareClick = () => {};
+	const [squares, setSquares] = useState<any>(defaultSquares);
+
+	console.table(
+		'squares', squares
+	)
+
   return (
     <Box border="2px solid black">
       <Box display="table" content="">
-        <Square value="c" onSquareClick={handleSquareClick} />
-        <Square value="c" onSquareClick={handleSquareClick} />
-        <Square value="X" onSquareClick={handleSquareClick} />
-        <Square value="c" onSquareClick={handleSquareClick} />
+        <Square value={squares[0][0]} i={0} j={0}  squares={squares} setSquares={setSquares} />
+        <Square value={squares[0][1]} i={0} j={1} squares={squares} setSquares={setSquares} />
+        <Square value={squares[0][2]} i={0} j={2} squares={squares} setSquares={setSquares}/>
+        <Square value={squares[0][3]} i={0} j={3} squares={squares} setSquares={setSquares} />
       </Box>
-      <Box display="table" content="">
-        <Square value="c" onSquareClick={handleSquareClick} />
-        <Square value="c" onSquareClick={handleSquareClick} />
-        <Square value="X" onSquareClick={handleSquareClick} />
-        <Square value="c" onSquareClick={handleSquareClick} />
+			<Box display="table" content="">
+        <Square value={squares[1][0]} i={1} j={0} squares={squares} setSquares={setSquares}  />
+        <Square value={squares[1][1]} i={1} j={1} squares={squares} setSquares={setSquares}  />
+        <Square value={squares[1][2]} i={1} j={2} squares={squares} setSquares={setSquares} />
+        <Square value={squares[1][3]} i={1} j={3} squares={squares} setSquares={setSquares} />
       </Box>
-      <Box display="table" content="">
-        <Square value="c" onSquareClick={handleSquareClick} />
-        <Square value="c" onSquareClick={handleSquareClick} />
-        <Square value="X" onSquareClick={handleSquareClick} />
-        <Square value="c" onSquareClick={handleSquareClick} />
+			<Box display="table" content="">
+        <Square value={squares[2][0]} i={2} j={0} squares={squares} setSquares={setSquares} />
+        <Square value={squares[2][1]} i={2} j={1} squares={squares} setSquares={setSquares}  />
+        <Square value={squares[2][2]} i={2} j={2} squares={squares} setSquares={setSquares} />
+        <Square value={squares[2][3]} i={2} j={3} squares={squares} setSquares={setSquares} />
       </Box>
-      <Box display="table" content="">
-        <Square value="c" onSquareClick={handleSquareClick} />
-        <Square value="c" onSquareClick={handleSquareClick} />
-        <Square value="X" onSquareClick={handleSquareClick} />
-        <Square value="c" onSquareClick={handleSquareClick} />
+			<Box display="table" content="">
+        <Square value={squares[3][0]} i={3} j={0} squares={squares} setSquares={setSquares} />
+        <Square value={squares[3][1]} i={3} j={1} squares={squares} setSquares={setSquares} />
+        <Square value={squares[3][2]} i={3} j={2} squares={squares} setSquares={setSquares} />
+        <Square value={squares[3][3]} i={3} j={3} squares={squares} setSquares={setSquares} />
       </Box>
     </Box>
   );
--- src/components/puzzle/square/index.tsx
+++ src/components/puzzle/square/index.tsx
@@ -1,18 +1,59 @@
-import React, { MouseEventHandler } from "react";
+import React, { MouseEventHandler, useEffect, useState } from "react";
 import { FaCheck } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 
 import styles from "../styles.module.scss";
 
 interface Props {
-  value: string;
-  onSquareClick: MouseEventHandler<HTMLButtonElement>;
+  value: number | null;
+	i: number;
+	j: number;
+	squares: any;
+	setSquares: Function;
+  // onSquareClick: Function;
 }
 
-const Square: React.FC<Props> = ({ value, onSquareClick }) => {
+const Square: React.FC<Props> = ({ value, i, j, squares, setSquares }) => {
+	const [clickTimes, setClickTimes] = useState(1);
+
+	const handleSquareClick = (i:number,j:number) => {
+		const newSquares = squares.slice()
+		if(clickTimes===1) {
+			newSquares[i][j]=0
+			setClickTimes(clickTimes+1)
+		}
+		if(clickTimes===2) {
+			newSquares[i][j]=1
+			for(let p=0; p<4; p++) {
+				if(p!=i) {
+					newSquares[p][j] = 0
+				}
+				if(p!=j) {
+					newSquares[i][p] = 0
+				}
+			}
+			setClickTimes(clickTimes+1)
+		}
+		if(clickTimes===3) {
+			newSquares[i][j]=null
+			for(let q=0; q<4; q++) {
+				newSquares[i][q]=null
+				newSquares[q][j]=null
+			}
+			setClickTimes(1)
+		}
+		setSquares(newSquares)
+	};
+
+	//console.log('value', value)
+	// useEffect(() => {
+	// 	console.log('changed')
+	// 	setClickTimes(1)
+	// }, [i,j])
   return (
-    <button className={styles.square} onClick={onSquareClick}>
-      {value === "X" ? <ImCross /> : <FaCheck />}
+    <button className={styles.square} onClick={() =>handleSquareClick(i,j)}>
+		{value!=null && (value==0 ? <ImCross/> : <FaCheck/>)}
+      {/* {value === "X" ? <ImCross /> : <FaCheck />} */}
     </button>
   );
 };
